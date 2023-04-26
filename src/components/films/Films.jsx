9661cf77cc2513cf4eee1fad8e4647b1c70d2ab3
toplus8 <juanto101@gmail.com>
2023-04-26 14:35:30 +0200

    Fisrt commit

---
+++ src/components/films/Films.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState } from 'react'
+import getData from '../../helpers/getData'
+
+
+const Films = () => {
+
+  const [films, setFilms]=useState([])  
+  return (
+    <div></div>
+  )
+}
+
+export default Films
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div>Best 10 films from www..com</div>
+  )
+}
+
+export default Nav
